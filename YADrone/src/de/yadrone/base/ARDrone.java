a9903ff17ea4e19370e68af67248b8397b9b4dd5
Formicarufa <Formicarufa@users.noreply.github.com>
2016-03-19 00:10:04 +0100

    Comment out the trick with setting emergency on before taking off.

--- YADrone/src/de/yadrone/base/ARDrone.java
+++ YADrone/src/de/yadrone/base/ARDrone.java
@@ -187,9 +187,10 @@ public class ARDrone implements IARDrone, IExceptionListener {
 	public void takeOff() {
 		if (commandManager != null)
 		{
+			
 			// we send emergency command before we take off which will toggle emergency mode off if the drone is actualy in emergency mode
 			// contributed by Naushad, see post on https://projects.ardrone.org/boards/1/topics/show/5259 from 30.04.2014
-	        commandManager.emergency();
+	        //commandManager.emergency(); //Formicarufa: Problem is if the drone was not in emergency mode, this toggles it on ;-)
 	        
 			commandManager.takeOff();
 		}
